dea08bc727fbe39b199d08e28292b9adbb6f66b6
changlin93 <767425412@qq.com>
2021-06-15 14:34:02 +0800

    feat: 搭建基本目录架构

---
+++ src/pages/found/Found.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Found extends Component {
+    render() {
+        return (
+            <View>
+                <Text>found</Text>
+            </View>
+        );
+    }
+}
